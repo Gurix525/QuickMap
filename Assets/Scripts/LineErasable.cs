0a2a6ade682a10753bd34c7dc72f999b7417fc64
Gurix525 <gurix525@gmail.com>
2023-09-26 04:16:47 +0200

    Removed editor reference

--- Assets/Scripts/LineErasable.cs
+++ Assets/Scripts/LineErasable.cs
@@ -1,4 +1,3 @@
-using UnityEditor;
 using UnityEngine;
 
 public class LineErasable : Erasable
@@ -22,7 +21,22 @@ public class LineErasable : Erasable
         _lineRenderer.GetPositions(nodes);
         if (Vector2.Distance(nodes[0], nodes[1]) < 0.5F)
             return true;
-        float distance = HandleUtility.DistancePointToLine(eraserPosition, nodes[0], nodes[1]);
+        float distance = GetDistanceFromPointToLine(eraserPosition, nodes[0], nodes[1]);
         return distance < 0.5F;
+    }
+
+    private float GetDistanceFromPointToLine(Vector2 eraserPosition, Vector3 start, Vector3 end)
+    {
+        float a = Vector2.Distance(start, end);
+        float b = Vector2.Distance(start, eraserPosition);
+        float c = Vector2.Distance(end, eraserPosition);
+        float s = (a + b + c) / 2F;
+        return 2 * Mathf.Sqrt(s * (s - a) * (s - b) * (s - c)) / a;
+
+        //a = dist(startp, endp)
+        //b = dist(startp, p)
+        //c = dist(endp, p)
+        //s = (a + b + c) / 2
+        //distance = 2 * sqrt(s(s - a)(s - b)(s - c)) / a
     }
 }
